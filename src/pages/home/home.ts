5e070e73743dbfcdd3a9e3351f5c24b3e3635e82
Sathish <ramsatt@gmail.com>
2017-07-24 11:50:17 +0530

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  Destination: any = '';
+  MyLocation: any;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  calculateAndDisplayRoute() {
+    let that = this;
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer;
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: {lat: 41.85, lng: -87.65}
+    });
+    directionsDisplay.setMap(map);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        map.setCenter(pos);
+        that.MyLocation = new google.maps.LatLng(pos);
+
+      }, function() {
+
+      });
+    } else {
+      // Browser doesn't support Geolocation
+    }
+
+    directionsService.route({
+    origin: this.MyLocation,
+    destination: this.Destination,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+
+}
